28e435592f3699dbdacd5d714ddda99c2f0f8bd0
Daniele Rondina <geaaru@sabayonlinux.org>
2021-08-12 19:33:54 +0200

    Reboot repo

--- multi-arch/packages/layers/kernel/mocaccino-5.4/sources/copy.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-set -ex
-PACKAGE_VERSION=${PACKAGE_VERSION%\+*}
-mkdir -p /luetbuild/sources/usr/src/
-mv ${KERNEL_TYPE} /luetbuild/sources/usr/src/${KERNEL_TYPE}-${PACKAGE_VERSION}-${SUFFIX}
